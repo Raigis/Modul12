0a43cbc1babd1dafb380ef4a29ac3c07a5cde6d8
Александр <146124268+Raigis@users.noreply.github.com>
2024-08-24 11:40:16 +0000

    Complete first task

--- FirstTask/main1.cpp
+++ FirstTask/main1.cpp
@@ -7,6 +7,27 @@
 
 #include <iostream>
 
+
+
 int main() {
-    
+    std::string residents[10];
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Enter the last name of the " << 1+i << " resident:\n";
+        std::getline(std::cin, residents[i]);
+    }
+
+    int numbers[3];
+    for (int i = 0; i < 3; i++) {
+        bool isCorrect;
+        do{
+            std::cout << "Enter the " << 1+i << " apartment number:\n";
+            std::cin >> numbers[i];
+            if (isCorrect = numbers[i] > 10 || numbers[i] < 1) std::cout << "Incorrect apartment number.\n";
+        } while (isCorrect);
+    }
+
+    for (int number : numbers) {
+        std::cout << "The resident of the apartment with the number " << number << " is " << residents[number-1] << std::endl;
+    }
+    return 0;
 }
